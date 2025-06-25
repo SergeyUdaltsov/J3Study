90eb89ebbd78dc74c2502d5194788262be9b6115
SergeyUdaltsov <sergii.udaltsov@gmail.com>
2025-06-25 17:50:50 +0300

    deployment files added

---
+++ src/main/java/com/study/J3Study/J3StudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.J3Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J3StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J3StudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/J3Study/J3StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.J3Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J3StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
